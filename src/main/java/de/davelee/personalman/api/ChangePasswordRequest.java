11ac2f51c8febadd2928c6903608907f3a316962
Dave Lee <dave@davelee.de>
2021-02-26 20:47:26 +0100

    Make a request object for changing password of a user

---
+++ src/main/java/de/davelee/personalman/api/ChangePasswordRequest.java
@@ -0,0 +1,28 @@
+package de.davelee.personalman.api;
+
+import lombok.*;
+
+/**
+ * This class is part of the PersonalMan REST API. It represents a request to change the password of a particular user.
+ * @author Dave Lee
+ */
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangePasswordRequest {
+
+    // username who's password should be changed
+    private String username;
+
+    // current password for this user
+    private String currentPassword;
+
+    // new password to set for this user
+    private String newPassword;
+
+    // company associated with
+    private String company;
+
+}
